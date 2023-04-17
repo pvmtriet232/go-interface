a01ca7d4d4d87c6b1a09ef7124cb1e14e0123956
triet <pvmtriet232@gmail.com>
2023-04-18 01:24:17 +0700

    another example of interface

--- main.go
+++ main.go
@@ -2,34 +2,50 @@ package main
 
 import "fmt"
 
+type Article struct {
+	Title  string
+	Author string
+}
+
+// defind a behavior
+type Stringer interface {
+	String() string
+}
+
+// defind a method with that method behavior, meaning you can
+// reuse this method via the Stringer interface without rewriting
+// your function
+func (a *Article) String() string {
+	return fmt.Sprintf("The %q article was written by %s.\n", a.Title, a.Author)
+}
+
+type Book struct {
+	Title  string
+	Author string
+	Page   int
+}
+
+func (b *Book) String() string {
+	return fmt.Sprintf("the %q book was written by %s.\n", b.Title, b.Author)
+}
+
 func main() {
-	An := &human{"Hello", 70}
-	Kiki := &animal{"gau gau", 4}
-	SaySomeThing(An)
-	SaySomeThing(Kiki)
-}
-func SaySomeThing(l listener) {
-	l.listen()
+	a := &Article{
+		Title:  "Understanding Interface in Go",
+		Author: "Sammy Shark",
+	}
+	Print(a)
+
+	b := &Book{
+		Title:  "The godfather",
+		Author: " Mario Puzo",
+		Page:   50,
+	}
+	Print(b)
+
 }
 
-type listener interface {
-	listen()
-}
-
-type animal struct {
-	sound  string
-	maxAge int
-}
-
-func (s *animal) listen() {
-	fmt.Println(s.sound)
-}
-
-type human struct {
-	sound  string
-	maxAge int
-}
-
-func (s *human) listen() {
-	fmt.Println(s.sound)
+// defind a function accept the interface, then pass the interface method
+func Print(s Stringer) {
+	fmt.Println(s.String())
 }
